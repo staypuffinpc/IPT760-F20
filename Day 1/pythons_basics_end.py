358f672a6c1ccdd73d0cd0d0046874440fd4770d
Peter Rich <pjrich@gmail.com>
2020-08-26 17:50:20 -0600

    Day 1 initial files

---
+++ Day 1/pythons_basics_end.py
@@ -0,0 +1,23 @@
+#### PURPOSE: demonstrate fundamental code in python ###
+
+## basic operations in python ##
+print (5 + 6) # addition
+print (5 - 6) # subtraction
+print (5.0 / 6) # division
+print (6 % 5) # displays the remainder
+
+print (4*5)
+
+# create a variable
+first = "Peter"
+last = "Rich"
+
+print (first + last)
+
+lights = True
+print (lights)
+
+#lists
+siblings = ["David", "Robyn", "Rebecca", "Michael","Peter","Dan","Matt", "Sarah","Liz", "Adam"]
+ages = [46, 45, 44, 42, 41, 39, 39, 36, 33, 29]
+print("I am " + siblings[4]+" and I am "+str(ages[4])+ " years old.")
---
+++ Day 1/pythons_basics_start.py
@@ -0,0 +1,28 @@
+#### PURPOSE: demonstrate fundamental code in python ###
+
+## basic OPERATIONS in python ##
+# addition
+# subtraction
+# division
+# displays the remainder
+
+#VARIABLES
+
+#LISTS
+
+
+## FUNCTIONS ##
+
+#define a function
+
+
+#call the function
+
+
+## CONDITIONALS ##
+# equivalency
+# inequalities
+# "not"
+
+
+## LOOPS ##
